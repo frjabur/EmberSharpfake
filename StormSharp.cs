df545c6d9c5293a710eee9c2783bb86aa7de84b7
frjabur <frjabur@gmail.com>
2017-07-01 00:26:02 -0300

    Update StormSharp.cs

--- StormSharp.cs
+++ StormSharp.cs
@@ -248,8 +248,8 @@ namespace StormSharpSDK
                 var l = (this.Owner.Distance2D(target));
                 var posA = this.Owner.Position;
                 var posB = target.Position;
-                var x = (posA.X + (l * posB.X)) / (1 + 30);
-                var y = (posA.Y + (l * posB.Y)) / (1 + 30);
+                var x = (posA.X - (l * posB.X)) / (1 + 1);
+                var y = (posA.Y - (l * posB.Y)) / (1 + 1);
                 var position = new Vector3((int)x, (int)y, posA.Z);
                 
                 //var l = (this.Owner.Distance2D(target) - sliderValue) / sliderValue;
